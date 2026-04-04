ff5ca26637b2704258ef2af4c7b135a277844229
Shreya Sengar <KhatarnakShreya@users.noreply.github.com>
2026-04-04 22:22:15 +0530

    Added function to add user

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ function showPage(page) {
 }
 
 showPage("students");
+
+
+
+// ADD STUDENT
+function addStudent() {
+  let s = {
+    name: sname.value,
+    cgpa: parseFloat(scgpa.value),
+    skills: sskills.value.toLowerCase().split(","),
+    year: parseInt(syear.value)
+  };
+
+  students.push(s);
+  localStorage.setItem("students", JSON.stringify(students));
+  displayStudents();
+
+  sname.value = "";
+  scgpa.value = "";
+  sskills.value = "";
+  syear.value = "";
+}
+
+
